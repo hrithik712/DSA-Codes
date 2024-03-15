f5a35fcf7013fe1db5f1f95c7707dc5830b60aba
Amandeep Mishra <68015433+hrithik712@users.noreply.github.com>
2024-03-15 14:34:16 +0530

    Add files via upload

--- reverseArray.cpp
+++ reverseArray.cpp
@@ -1,34 +1,44 @@
 #include<iostream>
+#include<vector>
 using namespace std;
-void reverse(int arr[], int n)
-{
-    int start =0;
-    int end = n-1;
 
-    while(start <= end)
-    {
-        swap(arr[start], arr[end]);
-        start++;
-        end--;
+vector<int> reverse(vector<int> v) {
+
+    int s = 0, e =v.size()-1;
+
+    while(s<=e) {
+        swap(v[s], v[e]);
+        s++;
+        e--;
     }
+
+    return v;
+
 }
-void printArray(int arr[], int n)
-{
-    for(int i=0;i<n;i++)
-    {
-        cout<<arr[i]<<" ";
+
+void print(vector<int> v) {
+
+    for(int i=0; i<v.size(); i++) {
+        cout << v[i] << " ";
     }
-    cout<<endl;
+    cout << endl;
 }
-int main()
-{
-    int arr[6] = {1,4,0,5,-2,15};
-    int brr[5] = {2, 6, 3, 9, 4};
 
-    reverse(arr, 6);
-    reverse(brr, 5);
+int main() {
 
-    printArray(arr, 6);
-    printArray(brr, 5);
+    vector<int> v ;
+
+    v.push_back(11);
+    v.push_back(7);
+    v.push_back(3);
+    v.push_back(12);
+    v.push_back(4);
+
+    vector<int> ans = reverse(v);
+
+    cout << "Printing reverse Array" <<endl;
+    print(ans);
+
+
     return 0;
 }
