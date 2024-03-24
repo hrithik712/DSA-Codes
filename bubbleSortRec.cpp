39e8e1fba0c2be63812e0487f3fe7b75fe6df18c
Amandeep Mishra <68015433+hrithik712@users.noreply.github.com>
2024-03-24 10:01:01 +0530

    Add files via upload

---
+++ bubbleSortRec.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+void bubbleSort(int *arr, int n)
+{
+    // base case ..
+    if(n == 0 || n == 1)
+    return;
+
+    for(int i=0; i<n-1; i++)
+    {
+        if(arr[i] > arr[i+1])
+        {
+            swap(arr[i], arr[i+1]);
+        }
+    }
+    bubbleSort(arr, n-1);
+}
+int main()
+{
+    int arr[5] = { 45, 31, 78, 11, 100};
+    bubbleSort(arr,5);
+
+    for(int i=0; i<5; i++)
+    {
+        cout<<arr[i]<<" "<<endl;
+    }
+    return 0;
+}
