4885bf9c2f49d6c555beaca61ade9e1ce24d1de1
Amandeep Mishra <68015433+hrithik712@users.noreply.github.com>
2024-06-08 18:56:43 +0530

    Add files via upload

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,43 +1,33 @@
+// Elements should be in shorted order then only binary search can be used.
+// O(log n) time taken in binary search. Hence it is faster than linear search.....
 #include<iostream>
 using namespace std;
+int main()
+{
+    int A[5]={23, 45, 56, 78, 89};
+    int low=0, high=9, key, mid;
 
-void print(int arr[], int s, int e) {
+    cout<<"Enter key to find in array - ";
+    cin>>key;
 
-    for(int i=s; i<=e; i++) {
-        cout << arr[i] << " ";
-    } cout << endl;
-}
-
-bool binarySearch(int *arr, int s, int e , int k ) {
-
-    //base case
-
-    //element not found
-    if(s>e)
-        return false;
-
-    int mid = s + (e-s)/2;
-
-    //element found
-    if(arr[mid] == k)
-        return true;
-
-    if(arr[mid] < k) {
-        return binarySearch(arr, mid+1, e, k);
+    while(low<=high)
+    {
+        mid=(low+high)/2;
+        if(key==A[mid])
+        {
+            cout<<"Found at index - "<<mid;
+            return 0;
+        }
+        else if (key<A[mid])
+        {
+            high=mid-1;
+        }
+        else
+        {
+            low=mid+1;
+        }
     }
-    else{
-        return binarySearch(arr, s, mid-1, k);
-    }
-}
-
-
-int main() {
-
-    int arr[11] = {2,4,6,10,14,18,22,38,49,55,222};
-    int size = 11;
-    int key = 222;
-
-    cout << "Present or not " << binarySearch(arr, 0, size-1, key) << endl;
-
+    cout<<"Key not found"<<endl;
     return 0;
+
 }
